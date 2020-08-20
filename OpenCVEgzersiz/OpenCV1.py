e4c6e795e661cfeca13939538b0a1c52070521b6
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-08-20 19:52:02 +0300

    20082020

--- OpenCVEgzersiz/OpenCV1.py
+++ OpenCVEgzersiz/OpenCV1.py
@@ -1,13 +1,12 @@
-import cv2 
-img =  cv2.imread(r"images\res1.jpg")
+import cv2
+import numpy as np
+
+img =  cv2.imread(r"images\res1.jpg",cv2.IMREAD_COLOR)
+
+px = img[250:300,250:300]
+img[350:400,350:400]= px
+img[250:300,250:300] = [1,1,1]
+
 cv2.imshow("Resim",img)
-print(img)
-print("img",img.shape)
-
-gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("Siyah Beyaz",gri)
-print(gri)
-print("gri",gri.shape)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ OpenCVEgzersiz/OpenCv3.py
@@ -0,0 +1,12 @@
+import cv2 
+img =  cv2.imread(r"images\res1.jpg")
+cv2.line(img,(50,50),(150,150),(255,100,100),15)
+cv2.rectangle(img,(50,50),(150,150),(255,120,100),1)
+cv2.circle(img,(100,100),55,(255,120,100),-1)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,"Uzaktan Egitim",(50,50),font,1,(200,255,155),2,cv2.LINE_AA)
+
+cv2.imshow("Resim",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
