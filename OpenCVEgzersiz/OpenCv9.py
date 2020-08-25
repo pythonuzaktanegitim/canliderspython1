0023e4b1ceb1c3234874ea3f319c6cad5cebf5ab
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-08-25 20:24:57 +0300

    25082020_2

--- OpenCVEgzersiz/OpenCv9.py
+++ OpenCVEgzersiz/OpenCv9.py
@@ -1,4 +1,25 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier()
+face_cascade = cv2.CascadeClassifier(r"cascades\haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(r"cascades\haarcascade_eye.xml")
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    yuzler = face_cascade.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in yuzler:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    
+    cv2.imshow("yuz",frame)
+    if cv2.waitKey(1) & 0xFF == ord("e"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
