7dbed64e5f9a3a789b116b2e427e15fce6b0dcb0
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-08-25 20:35:04 +0300

    25/08/2020_3

--- OpenCVEgzersiz/OpenCv9.py
+++ OpenCVEgzersiz/OpenCv9.py
@@ -10,10 +10,15 @@ while True:
     ret,frame = cap.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-    yuzler = face_cascade.detectMultiScale(gray,1.3,5)
+    yuzler = face_cascade.detectMultiScale(gray,1.3,8)
 
     for (x,y,w,h) in yuzler:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+        gozler = eye_cascade.detectMultiScale(roi_gray,1.3,3)
+        for (ex,ey,ew,eh) in gozler:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
     
     cv2.imshow("yuz",frame)
     if cv2.waitKey(1) & 0xFF == ord("e"):
