41a99c23d5362e97d4d7e8173bf302e7d212fd20
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-08-27 20:13:59 +0300

    27082020

--- arayuz/kamera.py
+++ arayuz/kamera.py
@@ -1,3 +1,61 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5.QtCore import QTimer
+from PyQt5 import uic
+import cv2
+from PyQt5.QtGui import QImage,QPixmap
+import numpy as np
+from PIL import Image
+import os
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.timer = QTimer()
+        self.devam = True
+        self.initUI()
+
+    def initUI(self):
+        self.win = uic.loadUi(r"arayuz\kamera.ui")
+        self.win.btKamera.clicked.connect(self.kameraAc)
+        self.win.btKapat.clicked.connect(self.Kapat)
+        self.win.show()
+
+
+    def kameraAc(self):
+        if not self.timer.isActive():
+            self.cap = cv2.VideoCapture(1)
+            self.timer.start(3)
+            self.Kamera()
+        else:
+            self.cap.release()
+            self.timer.stop()
+        
+
+    def Kapat(self):
+        if self.timer.isActive():
+            self.devam = False
+
+    def Kamera(self):
+        while self.devam:
+            ret,frame = self.cap.read()
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            height,width,channel = frame.shape  # (1024,768,3)
+            step = channel*width
+            qImg = QImage(frame.data,width,height,step,QImage.Format_RGB888)
+            self.win.lblKamera.setPixmap(QPixmap.fromImage(qImg))
+
+            if (cv2.waitKey(1) & 0xFF == ord("ç")):
+                break
+
+        self.win.lblKamera.setText("")
+        self.cap.release()
+        self.timer.stop()
+        
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    uyg = App()
+    sys.exit(app.exec_())
